4dd7a29e3206b507ef213d763f59113e26c496bb
mingyu <wkdgnl123@cau.ac.kr>
2026-04-28 12:52:47 +0900

    feat: leetcode, programmers 폴더 추가

---
+++ leetcode/week1/JMG.cpp
@@ -0,0 +1,9 @@
+/********************************************************************************/
+/* File Name         : JMG.cpp                                                  */
+/* By                : MinGyu Jeong                            KK  KK   BBBBB   */
+/* Algorithm         :                                         KK KK    BB  BB  */
+/* Time Complexity   :                                         KKKKK    BBBBB   */
+/* Space Complexity  :                                         KK KK    BB  BB  */
+/* Note              :                                         KK  KK   BBBBB   */
+/*                                                                              */
+/********************************************************************************/
