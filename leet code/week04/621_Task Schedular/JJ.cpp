33a242c9bda00ef722f52151b1db5c70ba0e0a2e
jjeong64 <jjeong2.001102@gmail.com>
2026-05-06 19:17:41 +0900

    [LC] JJ - Task Schedular 수정

--- leet code/week04/621_Task Schedular/JJ.cpp
+++ leet code/week04/621_Task Schedular/JJ.cpp
@@ -20,26 +20,55 @@ class Solution {
 public:
     int time = 0;
     int leastInterval(vector<char>& tasks, int n) {
-        int answer = 0;
+        priority_queue<pair<int, char>> ready; //실행할 수 있는 작업 목록 저장하는 큐
+        queue<tuple<int, char, int>> wait; //복귀시간, 작업 이름, 남은 횟수
+
         //맵에 task와 그 빈도 기록
         unordered_map<char, int> um;
         for(int i = 0; i < tasks.size(); i++)
             um[tasks[i]]++;
         
-        //map을 이용하여 우선순위 큐에 집어넣기
-        priority_queue<pair<int, char>> ready;
+        //map을 이용하여 레디큐(우선순위 큐)에 집어넣기
         for(auto i : um)
-            ready.push(make_pair(i.second, i.first));
+            ready.push({i.second, i.first});
 
-        while(!ready.empty()){
-            int tempFrequent = ready.top().first;
-            char tempTask = ready.top().second;
-            ready.pop();
-            tempFrequent--;
+        //작업이 다 끝날 때까지(ready, wait큐가 빌때까지) 작업 돌리기
+        //ready큐에서 가장 남은 작업 수가 많은 task를 실행. (ready큐에서 빠짐)
+        //frequent 수를 줄이고 0이 아니면 wait큐에 저장 후 time++
+        //wait의 task가 복귀 시간에 도달하면 ready큐로 이동
+        //ready가 비어있고 wait가 비어있지 않으면 idle 추가
 
+        //time = get<0>(wait.front());
+        while(!ready.empty() || !wait.empty()){
+            while(!wait.empty() && get<0>(wait.front()) <= time){
+                char tempTask = get<1>(wait.front());
+                int tempFrequent = get<2>(wait.front());
+                wait.pop();
+                ready.push({tempFrequent, tempTask});
+            }
+            
+            //레디큐가 비면 wait큐에서 ready로 올 수 있는 시간으로 스킵
+            if(ready.empty()){
+                time = get<0>(wait.front());
+                continue;
+            }
 
+            //레디큐에서 task 실행
+            if(!ready.empty()){
+                int tempFrequent = ready.top().first;
+                
+                char tempTask = ready.top().second;
+                ready.pop();
+
+                tempFrequent--;
+                if(tempFrequent != 0)
+                    wait.push({time + n + 1, tempTask, tempFrequent});
+                time++;
+            }
         }
+        return time;
     }
+    
 };
 
 int main(void)
