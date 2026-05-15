7d0804a1559ae21ff69fce27b889df8d48e674ab
jjeong64 <jjeong2.001102@gmail.com>
2026-05-16 08:19:25 +0900

    레포지 이름으로 인한 오류 해결

---
+++ leet code/week05/221_Maximal Square/JJ.cpp
@@ -0,0 +1,18 @@
+/********************************************************************************/
+/* File Name         : JJ.cpp                                                   */
+/* By                : jjeong2                                 KK  KK   BBBBB   */
+/* Algorithm         :                                         KK KK    BB  BB  */
+/* Time Complexity   :                                         KKKKK    BBBBB   */
+/* Space Complexity  :                                         KK KK    BB  BB  */
+/* Note              :                                         KK  KK   BBBBB   */
+/*                                                                              */
+/********************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    
+}
